ee1e0cd76d0bd907f7886af69982578f85416ede
Aaqei Aqeix <aaqei@Aaqeis-MacBook-Pro.local>
2024-05-13 00:43:42 +0500

    Markdown

--- src/app/(tabs)/[slug].tsx
+++ src/app/(tabs)/[slug].tsx
@@ -7,24 +7,44 @@ import Head from 'expo-router/head';
 import { ORIGIN } from '@/src/config';
 
 export async function generateStaticParams(): Promise<
-  Record<string, string>[]
+    Record<string, string>[]
 > {
-  const posts = getAllPosts();
+    const posts = getAllPosts();
 
-  return posts.map((post) => ({ slug: post.slug }));
+    return posts.map((post) => ({ slug: post.slug }));
 }
 
 const PostDetailsPage = () => {
-  const { slug } = useLocalSearchParams();
-  const [post, setPost] = useState(getPost(slug));
+    const { slug } = useLocalSearchParams();
+    const [post, setPost] = useState(getPost(slug));
 
-  if (!post) {
-    return <Text>Post not found</Text>;
-  }
+    if (!post) {
+        return <Text>Post not found</Text>;
+    }
 
-  return (
-   <Text>{post.title}</Text>
-  );
+    return (
+        <ScrollView
+
+            style={{
+
+                flex: 1,
+                backgroundColor: 'white'
+
+
+            }} contentContainerStyle={{
+                marginHorizontal: "auto",
+                width: '100%',
+                maxWidth: 960,
+                padding: 20
+            }}>
+
+
+            <Text style={{ fontSize: 30, marginBottom: 20 }}>{post.title}</Text>
+            <Markdown>{post.content}</Markdown>
+
+
+        </ScrollView>
+    );
 };
 
 export default PostDetailsPage;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -24,7 +24,7 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
    flex:1,
-   backgroundColor:"red"
+   
     
     
   },
